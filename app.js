d9fc26384c9e77741870d04cb3b281f7e8ddc544
stzvi <100715488+stzvi@users.noreply.github.com>
2022-06-05 09:42:29 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -37,7 +37,11 @@ function displayMovies(movies){
    movies.forEach(  (movie) => {
        
     moviesArray.push(`<tr>
-    <td>${movie.watched}</td>
+    <td>
+    <input type="checkbox" class="form-check-input ml-4" id="checkboxWatched">
+    <label class="form-check-label" for="watcheMovie"></label>
+
+    </td>
     <td>${movie.movieName}</td>
     <td>${movie.releaseYear}</td>
     <td>${movie.countryOfRelease}</td>
@@ -53,14 +57,9 @@ function displayMovies(movies){
 function addNewMovie(){
 
 
-    document.getElementById("addMovieButton").addEventListener('click',() =>{
-
-   
-
-        })
 
 
-    }
+}
 
 
 displayMovies(movieList);
